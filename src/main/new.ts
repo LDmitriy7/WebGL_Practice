172b322cf6bc121d6359595b1598b15db668ebc7
Dmitriy <levchenko.d.a1998@gmail.com>
2024-01-28 23:55:38 +0600

    -

--- src/main/new.ts
+++ src/main/new.ts
@@ -6,36 +6,21 @@ const gl = new Gl(_gl)
 const shaderSource = shaderSources.solid
 const program = gl.buildProgramFromSources(shaderSource)
 const locs = shaderSource.getLocations(gl.gl, program)
-const positionBuffer = gl.buildBuffer(locs.position)
 
-setRectPositions(0, 0, 100, 200)
-
-gl.setUniformVec4(locs.color, [1, 0, 0, 1])
 gl.setResolution(locs.resolution)
+gl.setUniformVec4(locs.color, [1, 0, 0, 1])
 
+const positionBuffer = gl.buildBuffer(locs.position)
+setRectPositions(0, 0, 100, 200)
 gl.drawTriangles(6)
-
 setRectPositions(300, 0, 100, 200)
-
 gl.drawTriangles(6)
 
 function setRectPositions(x: number, y: number, width: number, height: number) {
-  var x1 = x - width / 2
-  var x2 = x1 + width
-  var y1 = y - height / 2
-  var y2 = y1 + height
-  gl.fillBuffer(positionBuffer, [
-    x1,
-    y1,
-    x2,
-    y1,
-    x1,
-    y2,
-    x1,
-    y2,
-    x2,
-    y1,
-    x2,
-    y2,
-  ])
+  const x1 = x - width / 2
+  const x2 = x1 + width
+  const y1 = y - height / 2
+  const y2 = y1 + height
+  const data = [x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]
+  gl.fillBuffer(positionBuffer, data)
 }
