e5deb5837905d9c4cba9596540a4f1b2978c35c5
Dmitriy <levchenko.d.a1998@gmail.com>
2024-01-30 22:52:47 +0600

    -

---
+++ modules/shader.ts
@@ -0,0 +1,121 @@
+import { mat3 } from "gl-matrix"
+import { GlManager } from "./gl-manager"
+import { ShaderLocs, shaderSource } from "./shaderSource"
+import { Gl, Vec2, Vec4 } from "./gl-manager/types"
+
+export const RECT_COORDS: Vec2[] = [
+  [0.0, 1.0],
+  [1.0, 1.0],
+  [0.0, 0.0],
+  [0.0, 0.0],
+  [1.0, 1.0],
+  [1.0, 0.0],
+]
+
+type ShaderBuffers = {
+  textureCoord: WebGLBuffer
+  vertexPosition: WebGLBuffer
+}
+
+export class Shader {
+  program: WebGLProgram
+  private glm: GlManager
+  private textureUnit = 0
+  private texture: WebGLTexture
+  private locs: ShaderLocs
+  private buffers: ShaderBuffers
+  private _image?: HTMLImageElement
+  private _vertexPositions = RECT_COORDS.flat().map((i) => i - 0.5)
+  private _projectionMatrix = mat3.create()
+  private _color: Vec4 = [1, 1, 1, 1]
+  private _textureCoords = RECT_COORDS.flat()
+
+  constructor(gl: Gl) {
+    const glm = new GlManager(gl)
+    this.glm = glm
+    this.program = glm.buildProgram(shaderSource)
+    this.texture = glm.buildTexture()
+    const locs = shaderSource.getLocs(gl, this.program)
+    this.locs = locs
+    this.buffers = {
+      textureCoord: glm.buildBuffer(locs.textureCoord),
+      vertexPosition: glm.buildBuffer(locs.vertexPosition),
+    }
+    this.initUniforms()
+    this.fillTexture()
+    this.fillTextureCoordBuffer()
+    this.fillVertexPositionBuffer()
+  }
+
+  get image() {
+    return this._image
+  }
+  set image(value) {
+    this._image = value
+    this.fillTexture()
+  }
+
+  get vertexPositions() {
+    return this._vertexPositions
+  }
+  set vertexPositions(value) {
+    this._vertexPositions = value
+    this.fillVertexPositionBuffer()
+  }
+
+  get textureCoords() {
+    return this._textureCoords
+  }
+  set textureCoords(value) {
+    this._textureCoords = value
+    this.fillTextureCoordBuffer()
+  }
+
+  get projectionMatrix() {
+    return this._projectionMatrix
+  }
+  set projectionMatrix(value) {
+    this._projectionMatrix = value
+    this.updateProjectionMatrix()
+  }
+
+  get color(): Vec4 {
+    return this._color
+  }
+  set color(value: Vec4) {
+    this._color = value
+    this.updateColor()
+  }
+
+  private initUniforms() {
+    const { locs, glm } = this
+    glm.setUniformInt(locs.sampler, this.textureUnit)
+    this.updateColor()
+    this.updateProjectionMatrix()
+  }
+
+  private updateProjectionMatrix() {
+    const { locs, glm } = this
+    glm.setUniformMat3(locs.projectionMatrix, this.projectionMatrix)
+  }
+
+  private updateColor() {
+    const { locs, glm } = this
+    glm.setUniformVec4(locs.color, this.color)
+  }
+
+  private fillTexture() {
+    const { texture, glm } = this
+    glm.fillTexture(texture, this.image, this.textureUnit)
+  }
+
+  private fillTextureCoordBuffer() {
+    const { glm, buffers } = this
+    glm.fillBuffer(buffers.textureCoord, this.textureCoords)
+  }
+
+  private fillVertexPositionBuffer() {
+    const { glm, buffers } = this
+    glm.fillBuffer(buffers.vertexPosition, this.vertexPositions)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import "./loader"
 import { mat3 } from "gl-matrix"
-import { Gl, Vec2 } from "../modules/gl-manager/types"
-import { gl, glm } from "./loader"
+import { Vec2 } from "../modules/gl-manager/types"
+import { glm } from "./loader"
 import { image } from "./texture"
-import { ShaderLocs, shaderSource } from "../modules/shaderSource"
-import { GlManager } from "../modules/gl-manager"
-import { Vec4 } from "../_src/utils"
+import { RECT_COORDS, Shader } from "../modules/shader"
 
 const size: Vec2 = [240, 180]
 
@@ -30,107 +28,6 @@ const projectionMatrix = createProjectionMatrix(
   [1, 1]
 )
 
-export const RECT_COORDS: Vec2[] = [
-  [0.0, 1.0],
-  [1.0, 1.0],
-  [0.0, 0.0],
-  [0.0, 0.0],
-  [1.0, 1.0],
-  [1.0, 0.0],
-]
-
-class Shader {
-  private glm: GlManager
-  private _image?: HTMLImageElement | undefined
-  private _vertexPositions = RECT_COORDS.flat().map((i) => i - 0.5)
-  private _projectionMatrix = mat3.create()
-  textureUnit = 0
-  color: Vec4 = [1, 1, 1, 1]
-  program: WebGLProgram
-  texture: WebGLTexture
-  locs: ShaderLocs
-  buffers: {
-    textureCoord: WebGLBuffer
-    vertexPosition: WebGLBuffer
-  }
-  private _textureCoords = RECT_COORDS.flat()
-
-  constructor(gl: Gl) {
-    this.glm = new GlManager(gl)
-    this.program = glm.buildProgram(shaderSource)
-    this.texture = glm.buildTexture()
-    const locs = shaderSource.getLocs(gl, this.program)
-    this.locs = locs
-    this.buffers = {
-      textureCoord: glm.buildBuffer(locs.textureCoord),
-      vertexPosition: glm.buildBuffer(locs.vertexPosition),
-    }
-    this.initUniforms()
-    this.fillTexture()
-    this.fillTextureCoordBuffer()
-    this.fillVertexPositionBuffer()
-  }
-
-  get image() {
-    return this._image
-  }
-  set image(value) {
-    this._image = value
-    this.fillTexture()
-  }
-
-  get vertexPositions() {
-    return this._vertexPositions
-  }
-  set vertexPositions(value) {
-    this._vertexPositions = value
-    this.fillVertexPositionBuffer()
-  }
-
-  get textureCoords() {
-    return this._textureCoords
-  }
-  set textureCoords(value) {
-    this._textureCoords = value
-    this.fillTextureCoordBuffer()
-  }
-
-  get projectionMatrix() {
-    return this._projectionMatrix
-  }
-  set projectionMatrix(value) {
-    this._projectionMatrix = value
-    this.updateProjectionMatrix()
-  }
-
-  private initUniforms() {
-    const { locs, glm } = this
-    glm.setUniformVec4(locs.color, this.color)
-    glm.setUniformInt(locs.sampler, this.textureUnit)
-    this.updateProjectionMatrix()
-  }
-
-  private updateProjectionMatrix() {
-    const { locs, glm } = this
-    glm.setUniformMat3(locs.projectionMatrix, this.projectionMatrix)
-  }
-
-  private fillTexture() {
-    const { texture, glm } = this
-    glm.fillTexture(texture, this.image, this.textureUnit)
-  }
-
-  private fillTextureCoordBuffer() {
-    const { glm, buffers } = this
-    glm.fillBuffer(buffers.textureCoord, this.textureCoords)
-  }
-
-  private fillVertexPositionBuffer() {
-    const { glm, buffers } = this
-    glm.fillBuffer(buffers.vertexPosition, this.vertexPositions)
-  }
-}
-
 const shader = new Shader(glm.gl)
 const vertexPositions = RECT_COORDS.map((i) => [i[0] - 0.5, i[1] - 0.5])
   .map((i) => [i[0] * size[0], i[1] * size[1]])
@@ -139,6 +36,7 @@ const vertexPositions = RECT_COORDS.map((i) => [i[0] - 0.5, i[1] - 0.5])
 shader.vertexPositions = vertexPositions
 shader.projectionMatrix = projectionMatrix
 shader.textureCoords = shader.textureCoords.map((i) => i * 2)
+shader.color = [1, 1, 1, 0.5]
 
 function main() {
   shader.image = image
